61fbd57d3f504ff9eeec98565b67dadc65778389
Oleksandr Kulyk <kylik.aleksandr@gmail.com>
2021-06-13 15:55:27 -0400

    Add post page.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
@@ -10,14 +10,12 @@ router.get('/', async (req, res) => {
                 'text_content',
                 'creation_date',
             ],
-            include: [
-                {
-                    model: User,
-                    attributes: [
-                        'name',
-                    ],
-                },
-            ],
+            include: {
+                model: User,
+                attributes: [
+                    'name',
+                ],
+            },
         });
 
         const posts = postsData.map(post => post.get({ plain: true }));
@@ -42,6 +40,49 @@ router.get('/login', (req, res) => {
         }
 
         res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            attributes: {
+                exclude: [
+                    'id',
+                    'author_id',
+                ],
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: [
+                        'name',
+                    ],
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        'text',
+                        'creation_date',
+                    ],
+                    include: {
+                        model: User,
+                        attributes: [
+                            'name',
+                        ],
+                    },
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('postPage', {
+            post: post,
+            logged_in: req.session.logged_in,
+        });
     } catch (err) {
         res.status(500).json(err);
     }
